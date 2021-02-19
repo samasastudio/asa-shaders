4070bfaa550844ff6ae1186f77f8aa53ce725433
Sam Johnson <samj@iconcollective.com>
2021-02-19 01:28:36 -0600

    added glslCanvas to SlowBreaths

--- src/components/SlowBreaths.js
+++ src/components/SlowBreaths.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
-import './SlowBreaths.css';
+import React, { Component } from "react";
+import GlslCanvas from "glslCanvas";
+import "./SlowBreaths.css";
 
 export default class SlowBreaths extends Component {
+
+  componentDidMount() {
+    const canvas = document.querySelector("canvas");
+    const sandbox = new GlslCanvas(canvas);
+    sandbox.load("void main() { gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); }")
+  }
+
   render() {
     return (
-      <section>
-        <div className="inhale">Inhale Deeply</div>
-        <div className="exhale">Exhale Softly</div>
-      </section>
-    )
+      <div>
+        <canvas></canvas>
+        <section>
+          <div className="inhale">Inhale Deeply</div>
+          <div className="exhale">Exhale Softly</div>
+        </section>
+      </div>
+    );
   }
 }
