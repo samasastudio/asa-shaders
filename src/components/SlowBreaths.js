e539f58f329c6e3fb55d768446180a8e817fd02f
Sam Johnson <samj@iconcollective.com>
2021-02-19 02:06:26 -0600

    successfully added test shader with time working

--- src/components/SlowBreaths.js
+++ src/components/SlowBreaths.js
@@ -2,12 +2,30 @@ import React, { Component } from "react";
 import GlslCanvas from "glslCanvas";
 import "./SlowBreaths.css";
 
-export default class SlowBreaths extends Component {
+const frag = `#ifdef GL_ES
+precision highp float;
+#endif
 
+uniform float u_time;
+
+varying vec2 v_texcoord;
+
+void main(void)
+{
+    vec2 uv = -1. + 2. * v_texcoord;
+    gl_FragColor = vec4(
+        abs(sin(cos(u_time+3.*uv.y)*2.*uv.x+u_time)),
+        abs(cos(sin(u_time+2.*uv.x)*3.*uv.y+u_time)),
+        1.0,
+        1.0);
+}`;
+
+export default class SlowBreaths extends Component {
   componentDidMount() {
     const canvas = document.querySelector("canvas");
     const sandbox = new GlslCanvas(canvas);
-    sandbox.load("void main() { gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); }")
+    sandbox.setUniform("u_texture", "displacement1.jpg")
+    sandbox.load(frag);
   }
 
   render() {
