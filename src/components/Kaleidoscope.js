c2d91d22f5ef2571cb3dbfadbea9f7e805e6c02b
Sam Johnson <samj@iconcollective.com>
2021-02-21 22:38:52 -0600

    added image change onClick and adjusted point aspect ratio in glsl

--- src/components/Kaleidoscope.js
+++ src/components/Kaleidoscope.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import GlslCanvas from "glslCanvas";
 import "./Kaleidoscope.css";
-import Image from "../assets/images/trails.jpg";
+import Image1 from "../assets/images/trails.jpg";
+import Image2 from "../assets/images/flowers.jpg";
+import Image3 from "../assets/images/light.jpg";
 
 const frag = `
 #ifdef GL_ES
@@ -24,9 +26,11 @@ void main(void)
     vec2 uv = v_texcoord;
     uv *= 2.0;
     uv -= 1.0;
+
+    vec2 mouse = u_mouse / u_resolution;
     
     //angle and radius
-    float radius = length(uv);
+    float radius = length(uv) * mix(1.0, 2.0, mouse.x);
     float angle = atan(uv.y, uv.x);
     
     angle /= PI * 2.0;
@@ -38,11 +42,13 @@ void main(void)
         angle = 1.0 - fract(angle);
     }
     
-    angle += u_time;
+    angle += u_time * 0.1;
+    angle += mouse.y;
     angle /= SEGMENTS;
     angle *= PI * 2.0;
     
     vec2 point = vec2(radius * cos(angle), radius * sin(angle));
+    point *= vec2(1.0, 0.666);
     point = fract(point);
     
     vec4 color = texture2D(image, point);
@@ -51,51 +57,72 @@ void main(void)
 }`;
 
 export default class Kaleidoscope extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      images: [Image1, Image2, Image3],
+      sandbox: null,
+    };
+
+    this.nextImage = this.nextImage.bind(this);
+  }
+
   componentDidMount() {
     const canvas = document.querySelector("canvas");
     const sandbox = new GlslCanvas(canvas);
 
-    const calcSize = () => {
-      let ww = window.innerWidth;
-      let wh = window.innerHeight;
-      let dpi = window.devicePixelRatio;
-    
-      let s = Math.max(wh, ww);
-    
-      canvas.width = s + dpi;
-      canvas.height = s + dpi;
+    this.setState({ sandbox }, () => {
+      const calcSize = () => {
+        let ww = window.innerWidth;
+        let wh = window.innerHeight;
+        let dpi = window.devicePixelRatio;
 
-      canvas.style.width = s + "px";
-      canvas.style.height = s + "px";
-    }
+        let s = Math.max(wh, ww + 500);
 
-    calcSize();
+        canvas.width = s + dpi;
+        canvas.height = s + dpi;
 
-    window.addEventListener("resize", () => {
+        canvas.style.width = s + "px";
+        canvas.style.height = s + "px";
+      };
+
       calcSize();
-    })
-    
-    sandbox.load(frag);
-    sandbox.setUniform("image", Image);
+
+      window.addEventListener("resize", () => {
+        calcSize();
+      });
+
+      sandbox.load(frag);
+      this.nextImage();
+    });
+  }
+
+  nextImage() {
+    const { images, sandbox } = this.state;
+
+    const next = images.shift();
+    sandbox.setUniform("image", next);
+    images.push(next);
   }
 
   render() {
     return (
       <div className="kaleidoscope">
         <div className="canvas-holder">
-          <canvas></canvas>
+          <canvas onClick={this.nextImage}></canvas>
         </div>
         <div className="info">
-        <header>
-          <h1>Sam Asa Johnson</h1>
-          <p>Software Engineer // Sound Designer</p>
-        </header>
-        <nav>
-          <a href="#">Instagram</a>
-          <a href="#">Unsplash</a>
-          <a href="#">500px</a>
-        </nav>
-      </div>
+          <header>
+            <h1>Sam Asa Johnson</h1>
+            <p>Software Engineer // Sound Designer</p>
+          </header>
+          <nav>
+            <a href="#">Instagram</a>
+            <a href="#">Unsplash</a>
+            <a href="#">500px</a>
+          </nav>
+        </div>
       </div>
     );
   }
