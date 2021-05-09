b53368841726bceb1ad01eb78bf8a68a3bedd9f1
Sam Johnson <samj@iconcollective.com>
2021-05-08 23:35:25 -0500

    added conditional p for desktop

--- src/components/LuridDream.js
+++ src/components/LuridDream.js
@@ -11,13 +11,11 @@ export const LuridDream = () => {
   return (
     <div className="luridDream">
       <main>
-        <header style={{display: 'flex'}}>
+        <header>
           <div style={{width: '50%', padding: '32px'}}>
             <h1>Sam Johnson</h1>
             <h2>Software Engineer</h2>
-          </div>
-          <div style={{width: '50%', textAlign: 'right',padding: '32px', backgroundColor: 'RGBA(255, 242, 234, 0.25)'}}>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac ullamcorper sapien, nec ullamcorper risus. Mauris pretium ex lectus, quis mollis nulla lobortis sed. Donec pellentesque blandit lacus vel tristique.</span>
+            {window.innerWidth > 410 ? <p>Click to cycle web shaders</p> : null}
           </div>
         </header>
         <footer>
