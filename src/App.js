88723846e53f3dc110ff80627929e0314904ab51
Sam Johnson <samj@iconcollective.com>
2021-05-08 19:30:02 -0500

    added ASA in image of FBM

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import "./App.css";
 import { Kaleidoscope } from "./components/Kaleidoscope";
 import { SlowBreaths } from "./components/SlowBreaths";
 import { FBM } from "./components/FBM";
+import { ASA } from "./components/ASA";
 
 function App() {
-  return <SlowBreaths />;
+  return <ASA />;
 }
 
 export default App;
---
+++ src/components/ASA.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import "./ASA.css";
+import { renderFBM } from "../fragments/fbmFrag"
+
+export const ASA = () => {
+  useEffect(renderFBM, []);
+  return (
+    <div className="fbm">
+      <main>
+        <header>
+          <h1>Sam Johnson</h1>
+          <h2>Software Engineer</h2>
+        </header>
+        <footer>
+          <a href="#">Projects</a>
+          <a href="#">About</a>
+          <a href="#">Github</a>
+        </footer>
+      </main>
+    </div>
+  );
+};
