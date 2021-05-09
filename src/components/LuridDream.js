7b104f63368bd0080da567ada81b752c8ce28b56
Sam Johnson <samj@iconcollective.com>
2021-05-08 23:15:29 -0500

    added lorem ipsum and backdrop styling for left div

--- src/components/LuridDream.js
+++ src/components/LuridDream.js
@@ -12,12 +12,12 @@ export const LuridDream = () => {
     <div className="luridDream">
       <main>
         <header style={{display: 'flex'}}>
-          <div style={{width: '50%'}}>
+          <div style={{width: '50%', padding: '32px'}}>
             <h1>Sam Johnson</h1>
             <h2>Software Engineer</h2>
           </div>
-          <div style={{width: '50%', textAlign: 'right', backgroundColor: 'RGBA(255, 242, 234, 0.25)'}}>
-            <span>hello</span>
+          <div style={{width: '50%', textAlign: 'right',padding: '32px', backgroundColor: 'RGBA(255, 242, 234, 0.25)'}}>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac ullamcorper sapien, nec ullamcorper risus. Mauris pretium ex lectus, quis mollis nulla lobortis sed. Donec pellentesque blandit lacus vel tristique.</span>
           </div>
         </header>
         <footer>
