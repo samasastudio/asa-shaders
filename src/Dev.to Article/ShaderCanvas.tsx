1eabe18fe07d00296f356613fd7943cb41da950c
Sam Johnson <samj@iconcollective.com>
2021-05-17 19:17:32 -0500

    setting canvas h/w correctly using input value

--- src/Dev.to Article/ShaderCanvas.tsx
+++ src/Dev.to Article/ShaderCanvas.tsx
@@ -1,21 +1,29 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
+import {windowSizer } from "../utils/windowSizer"
 import GlslCanvas from 'glslCanvas';
 
 interface CanvasProps {
-  height: number | string;
-  width: number | string;
+  height?: number | string;
+  width?: number | string;
   frag: string;
 }
 
 export const ShaderCanvas: FC<CanvasProps> = (props): JSX.Element => {
 
+  const canvasRef = useRef();
+
   useEffect(() => {
-    const canvas = document.querySelector("canvas");
-    const sandbox = new GlslCanvas(canvas);
+
+    const node = canvasRef.current as any;
+    node.width = 500 + window.devicePixelRatio;
+    node.height = 500 + window.devicePixelRatio;
+    node.style.width = "500px";
+    node.style.height = "500px";
+    const sandbox = new GlslCanvas(canvasRef.current);
     sandbox.load(props.frag);
   }, [])
 
   return (
-    <canvas style={{width: props.width, height: props.height}}></canvas>
+    <canvas ref={canvasRef}></canvas>
   )
 }
